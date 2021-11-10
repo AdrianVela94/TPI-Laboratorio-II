f2ebae0089c423a466e77b8856c7fa7397be1c65
AdrianVela94 <94022556+AdrianVela94@users.noreply.github.com>
2021-11-10 11:46:17 -0300

    Add files via upload

---
+++ FrmSistAcadem.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class FrmSistAcadem
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSistAcadem));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmConsultas = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmConsulta1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmConsulta2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmConsulta3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmConsulta4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmVistas = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmVista1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmVista2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmProcedimientos = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSP1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSP2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.dgvReporte = new System.Windows.Forms.DataGridView();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lstDescripcion = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmConsultas,
+            this.tsmVistas,
+            this.tsmProcedimientos});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(688, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmConsultas
+            // 
+            this.tsmConsultas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmConsulta1,
+            this.tsmConsulta2,
+            this.tsmConsulta3,
+            this.tsmConsulta4});
+            this.tsmConsultas.Name = "tsmConsultas";
+            this.tsmConsultas.Size = new System.Drawing.Size(71, 20);
+            this.tsmConsultas.Text = "Consultas";
+            // 
+            // tsmConsulta1
+            // 
+            this.tsmConsulta1.Name = "tsmConsulta1";
+            this.tsmConsulta1.Size = new System.Drawing.Size(180, 22);
+            this.tsmConsulta1.Text = "Consulta 4";
+            this.tsmConsulta1.Click += new System.EventHandler(this.tsmConsulta1_Click_1);
+            // 
+            // tsmConsulta2
+            // 
+            this.tsmConsulta2.Name = "tsmConsulta2";
+            this.tsmConsulta2.Size = new System.Drawing.Size(180, 22);
+            this.tsmConsulta2.Text = "Consulta 6";
+            this.tsmConsulta2.Click += new System.EventHandler(this.tsmConsulta2_Click_1);
+            // 
+            // tsmConsulta3
+            // 
+            this.tsmConsulta3.Name = "tsmConsulta3";
+            this.tsmConsulta3.Size = new System.Drawing.Size(180, 22);
+            this.tsmConsulta3.Text = "Consulta 7";
+            this.tsmConsulta3.Click += new System.EventHandler(this.tsmConsulta3_Click_1);
+            // 
+            // tsmConsulta4
+            // 
+            this.tsmConsulta4.Name = "tsmConsulta4";
+            this.tsmConsulta4.Size = new System.Drawing.Size(180, 22);
+            this.tsmConsulta4.Text = "Consulta 8";
+            this.tsmConsulta4.Click += new System.EventHandler(this.tsmConsulta4_Click);
+            // 
+            // tsmVistas
+            // 
+            this.tsmVistas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmVista1,
+            this.tsmVista2});
+            this.tsmVistas.Name = "tsmVistas";
+            this.tsmVistas.Size = new System.Drawing.Size(49, 20);
+            this.tsmVistas.Text = "Vistas";
+            // 
+            // tsmVista1
+            // 
+            this.tsmVista1.Name = "tsmVista1";
+            this.tsmVista1.Size = new System.Drawing.Size(180, 22);
+            this.tsmVista1.Text = "Consulta 1";
+            this.tsmVista1.Click += new System.EventHandler(this.tsmVista1_Click_1);
+            // 
+            // tsmVista2
+            // 
+            this.tsmVista2.Name = "tsmVista2";
+            this.tsmVista2.Size = new System.Drawing.Size(180, 22);
+            this.tsmVista2.Text = "Consulta 3";
+            this.tsmVista2.Click += new System.EventHandler(this.tsmVista2_Click_1);
+            // 
+            // tsmProcedimientos
+            // 
+            this.tsmProcedimientos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmSP1,
+            this.tsmSP2});
+            this.tsmProcedimientos.Name = "tsmProcedimientos";
+            this.tsmProcedimientos.Size = new System.Drawing.Size(37, 20);
+            this.tsmProcedimientos.Text = "SPs";
+            // 
+            // tsmSP1
+            // 
+            this.tsmSP1.Name = "tsmSP1";
+            this.tsmSP1.Size = new System.Drawing.Size(180, 22);
+            this.tsmSP1.Text = "Consulta 2";
+            this.tsmSP1.Click += new System.EventHandler(this.tsmSP1_Click_1);
+            // 
+            // tsmSP2
+            // 
+            this.tsmSP2.Name = "tsmSP2";
+            this.tsmSP2.Size = new System.Drawing.Size(180, 22);
+            this.tsmSP2.Text = "Consulta 5";
+            this.tsmSP2.Click += new System.EventHandler(this.tsmSP2_Click_1);
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblDescripcion.Location = new System.Drawing.Point(12, 35);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(91, 16);
+            this.lblDescripcion.TabIndex = 2;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // dgvReporte
+            // 
+            this.dgvReporte.AllowUserToAddRows = false;
+            this.dgvReporte.AllowUserToDeleteRows = false;
+            this.dgvReporte.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvReporte.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.dgvReporte.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvReporte.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporte.Location = new System.Drawing.Point(39, 124);
+            this.dgvReporte.Name = "dgvReporte";
+            this.dgvReporte.ReadOnly = true;
+            this.dgvReporte.RowHeadersVisible = false;
+            this.dgvReporte.Size = new System.Drawing.Size(601, 248);
+            this.dgvReporte.TabIndex = 4;
+            this.dgvReporte.TabStop = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpiar.Location = new System.Drawing.Point(164, 410);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(90, 28);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar Grilla";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(601, 407);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 28);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnMostrar.Location = new System.Drawing.Point(22, 410);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(117, 28);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar Consulta";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lstDescripcion
+            // 
+            this.lstDescripcion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lstDescripcion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstDescripcion.FormattingEnabled = true;
+            this.lstDescripcion.Location = new System.Drawing.Point(12, 67);
+            this.lstDescripcion.Name = "lstDescripcion";
+            this.lstDescripcion.Size = new System.Drawing.Size(664, 39);
+            this.lstDescripcion.TabIndex = 7;
+            this.lstDescripcion.TabStop = false;
+            // 
+            // FrmSistAcadem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.ClientSize = new System.Drawing.Size(688, 450);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lstDescripcion);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.dgvReporte);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmSistAcadem";
+            this.Text = "Sistema Academico";
+            this.Load += new System.EventHandler(this.frmITV_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmConsultas;
+        private System.Windows.Forms.ToolStripMenuItem tsmConsulta1;
+        private System.Windows.Forms.ToolStripMenuItem tsmConsulta2;
+        private System.Windows.Forms.ToolStripMenuItem tsmConsulta3;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.DataGridView dgvReporte;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ToolStripMenuItem tsmVistas;
+        private System.Windows.Forms.ToolStripMenuItem tsmProcedimientos;
+        private System.Windows.Forms.ToolStripMenuItem tsmVista1;
+        private System.Windows.Forms.ToolStripMenuItem tsmVista2;
+        private System.Windows.Forms.ToolStripMenuItem tsmSP1;
+        private System.Windows.Forms.ToolStripMenuItem tsmSP2;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lstDescripcion;
+        private System.Windows.Forms.ToolStripMenuItem tsmConsulta4;
+    }
+}
+
---
+++ FrmSistAcadem.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+//NUEVO
+namespace WindowsFormsApp1
+{
+    public partial class FrmSistAcadem : Form
+    {
+        SqlConnection conexion = new SqlConnection(@"Data Source=DESKTOP-ROFF2B8\SQLEXPRESS;Initial Catalog=SistemaAcademico;Integrated Security=True");
+        SqlCommand comando = new SqlCommand();
+        string consulta;
+
+        public FrmSistAcadem()
+        {
+            InitializeComponent();
+
+        }
+
+        private void frmITV_Load(object sender, EventArgs e)
+        {
+            btnMostrar.Enabled = false;
+            btnLimpiar.Enabled = false;
+                       
+        }
+
+        private void cargarDetalle(string detalle)
+        {
+            lstDescripcion.Items.Clear();
+            lstDescripcion.Items.Add(detalle);
+            btnMostrar.Enabled = true;
+        }
+
+        private DataTable consultarTabla(string consulta)
+        {
+            DataTable tabla = new DataTable();
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = consulta;
+            tabla.Load(comando.ExecuteReader());
+            conexion.Close();
+            return tabla;
+        }
+
+   
+
+        //Botones
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            DataTable tabla = consultarTabla(consulta);
+            dgvReporte.DataSource = tabla;
+            dgvReporte.ClearSelection();
+            btnLimpiar.Enabled = true;
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            DialogResult limpiar = MessageBox.Show("Desea limpiar el informe?", "Limpiar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if (limpiar == DialogResult.Yes)
+            {
+                dgvReporte.DataSource = "";
+                consulta = "";
+                cargarDetalle("");
+                btnMostrar.Enabled = false;
+                btnLimpiar.Enabled = false;
+            }
+            else if (limpiar == DialogResult.No)
+            {
+                return;
+            }
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult salir = MessageBox.Show("Desea terminar el programa?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (salir == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else if (salir == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+
+        //CONSULTAS HECHAS CON VISTAS
+        //Consulta 1- ESTE HAY QUE CAMBIAR PARA QUE SEA UNA VISTA
+        //no lo hice porque le agregué un group by a la consulta, 1ero queria que la vieran
+        private void tsmVista1_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("1- Promedio de las notas parciales de los alumnos del corriente año:");
+            consulta = "select a.legajo Legajo, a.ape_alumno + ' ' + a.nom_alumno as 'Alumno', avg(nota) Nota" +
+                " from examenes e join alumnos a on a.legajo = e.legajo" +
+                " where year(fecha) = year(getdate()) and id_tipo = 1" +
+                " group by a.legajo, a.ape_alumno, a.nom_alumno";
+        }
+        //Consulta 3
+        private void tsmVista2_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("3- Consultar alumnos que estudien Ingeniera Civil:");
+            consulta = "select * from alumnos_civil";
+        }
+
+        //CONSULTAS 'NORMALES'
+        //Consulta 4--Esta la tenemos que arreglar
+        private void tsmConsulta1_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("4- Consultar alumnos con inasistencias en este año de la materia de química.");
+            consulta = "select a.legajo Legajo, a.nom_alumno + a.ape_alumno as 'Alumno'" +
+                " from examenes e join alumnos a on a.legajo = e.legajo" +
+                " join materias m on m.id_materia = e.id_materia" +
+                " where id_tipo = 3 and nota = 0 and year(fecha)= year(getdate())" +
+                " and m.nom_materia like 'Quimica'" +
+                " order by Alumno";
+        }
+        //Consulta 6
+        private void tsmConsulta2_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("6- Emitir un listado de los alumnos que se hayan" +
+                " matriculado este año incluyendo solo a aquellos cuyo legajo oscile" +
+                " entre 110000 y 115000.");
+            consulta = "select legajo 'Legajo', nom_alumno + ape_alumno as 'Alumno'" +
+                " from alumnos " +
+                "where legajo in(select m.legajo" +
+                               " from alumnos a join matriculas m" +
+                               " on a.legajo = m.legajo" +
+                               " where year(fecha)= year(getdate()))" +
+                               " and legajo between 110000 and 115000";
+        }
+
+        //Consulta 7 --A ESTE LE MANDÉ UN SELECT AL AZAR, HAY QUE ARRREGLAR LA CONSULTA
+        private void tsmConsulta3_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("7- Consultar las materias a las que está inscrito un alumno" 
+                );
+            consulta = "select a.legajo as 'Legajo', a.nom_alumno+' '+a.ape_alumno as 'Nombre'," +
+                " mate.nom_materia as 'Materia' from matriculasXmaterias mxm" +
+                " join materias mate on mate.id_materia= mxm.id_materia" +
+                " join matriculas matr on mxm.id_matricula= matr.id_matricula" +
+                " join alumnos a on matr.legajo= a.legajo" +
+                " order by 1";
+        }
+
+
+        //Consulta 8
+        private void tsmConsulta4_Click(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("8- Listado de docentes y alumnos de la utn:");
+            consulta = "select legajo as 'Legajo', nom_alumno+' '+ape_alumno as 'Nombre'," +
+                " 'Alumno' as 'Tipo'" +
+                " from alumnos" +
+                " union" +
+                " select legajo, nom_docente + ' ' + ape_docente, 'Docente'" +
+                " from docentes" +
+                " order by 3";
+        }
+
+        //PROCEDIMIENTOS ALMACENADOS
+        //Consulta 2-Falta terminar, muestra algo al azar
+        private void tsmSP1_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("2- Consultar alumnos en un rango de edad determinado:");
+            consulta = "select * from alumnos_civil";
+        }
+
+        //Consulta 5-falta terminar, muestra algo al azar
+        private void tsmSP2_Click_1(object sender, EventArgs e)
+        {
+            dgvReporte.DataSource = "";
+            cargarDetalle("5- Consultar lista de alumnos que aprobaron finales" +
+                " en el corriente año de una materia ingresada");
+            consulta = "select * from alumnos_civil";
+        }
+    }   
+        
+}
